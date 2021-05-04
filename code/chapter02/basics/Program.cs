aa8662ccb41183b3673783eb8c964307cb852143
Nick S <staunw01@pfw.edu>
2021-05-04 11:37:06 -0400

    finished chapter 12

--- code/chapter02/basics/Program.cs
+++ code/chapter02/basics/Program.cs
@@ -9,32 +9,38 @@ namespace basics
         static void Main(string[] args)
         {
 
-            System.Data.DataSet ds;
-            System.Net.Http.HttpClient client;
+            // System.Data.DataSet ds;
+            // System.Net.Http.HttpClient client;
 
-            
+            for (; ; )
+            {
+
+            }
+
+            // double heightInMeters = 1.88;
+            // Console.WriteLine($"The variable {nameof(heightInMeters)} has the value {heightInMeters}.");
 
             // loop through the assemblies that this app references
-            foreach (var r in Assembly.GetEntryAssembly()
-            .GetReferencedAssemblies())
-            {
-                // load the assembly so we can read its details
-                var a = Assembly.Load(new AssemblyName(r.FullName));
-                // declare a variable to count the number of methods
-                int methodCount = 0;
-                // loop through all the types in the assembly
-                foreach (var t in a.DefinedTypes)
-                {
-                // add up the counts of methods
-                methodCount += t.GetMethods().Count();
-                }
-                // output the count of types and their methods
-                Console.WriteLine(
-                "{0:N0} types with {1:N0} methods in {2} assembly.",
-                arg0: a.DefinedTypes.Count(),
-                arg1: methodCount,
-                arg2: r.Name);
-            }
+            // foreach (var r in Assembly.GetEntryAssembly()
+            // .GetReferencedAssemblies())
+            // {
+            //     // load the assembly so we can read its details
+            //     var a = Assembly.Load(new AssemblyName(r.FullName));
+            //     // declare a variable to count the number of methods
+            //     int methodCount = 0;
+            //     // loop through all the types in the assembly
+            //     foreach (var t in a.DefinedTypes)
+            //     {
+            //     // add up the counts of methods
+            //     methodCount += t.GetMethods().Count();
+            //     }
+            //     // output the count of types and their methods
+            //     Console.WriteLine(
+            //     "{0:N0} types with {1:N0} methods in {2} assembly.",
+            //     arg0: a.DefinedTypes.Count(),
+            //     arg1: methodCount,
+            //     arg2: r.Name);
+            // }
         }
     }
 }
