428d6a4a73766f964f4f6b60430db02eea613c21
Nick S <staunw01@pfw.edu>
2021-05-04 17:30:04 -0400

    finished chapter 13

---
+++ code/chapter13/AsyncEnumerable/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace AsyncEnumerable
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await foreach (int number in GetNumbers())
+            {
+                WriteLine($"Number: {number}");
+            }
+        }
+
+        static async IAsyncEnumerable<int> GetNumbers()
+        {
+            var r = new Random();
+
+            // simulate work
+            // System.Threading.Thread.Sleep(r.Next(1000, 2000));
+            // yield return r.Next(0, 101);
+
+            // System.Threading.Thread.Sleep(r.Next(1000, 2000));
+            // yield return r.Next(0, 101);
+
+            // System.Threading.Thread.Sleep(r.Next(1000, 2000));
+            // yield return r.Next(0, 101);
+
+            await Task.Run(() => Task.Delay(r.Next(1500, 3000)));
+            yield return r.Next(0, 101);
+
+            await Task.Run(() => Task.Delay(r.Next(1500, 3000)));
+            yield return r.Next(0, 101);
+
+            await Task.Run(() => Task.Delay(r.Next(1500, 3000)));
+            yield return r.Next(0, 101);
+        }
+    }
+}
---
+++ code/chapter13/SynchronizingResourceAccess/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using static System.Console;
+
+namespace SynchronizingResourceAccess
+{
+    class Program
+    {
+        static Random r = new Random();
+        static string Message; // a shared resource
+        static object c = new object();
+
+        static int Counter; // another shared resource
+
+        static void Main(string[] args)
+        {
+            WriteLine("Please wait for the tasks to complete.");
+            Stopwatch watch = Stopwatch.StartNew();
+
+            Task a = Task.Factory.StartNew(MethodA);
+            Task b = Task.Factory.StartNew(MethodB);
+
+            Task.WaitAll(new Task[] { a, b });
+
+            WriteLine();
+            WriteLine($"Results: {Message}.");
+            WriteLine($"{watch.ElapsedMilliseconds:#,##0} elapsed milliseconds.");
+            WriteLine($"{Counter} string modifications.");
+        }
+
+        static void MethodA()
+        {
+
+            try
+            {
+                Monitor.TryEnter(c, TimeSpan.FromSeconds(15));
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(r.Next(2000));
+                    Message += "A";
+                    Interlocked.Increment(ref Counter);
+                    Write(".");
+                }
+            }
+            finally
+            {
+                Monitor.Exit(c);
+            }
+            /*
+            lock (c)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(r.Next(2000));
+                    Message += "A";
+                    Write(".");
+                }
+            }
+            */
+        }
+
+        static void MethodB()
+        {
+            try
+            {
+                Monitor.TryEnter(c, TimeSpan.FromSeconds(15));
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(r.Next(2000));
+                    Message += "B";
+                    Interlocked.Increment(ref Counter);
+                    Write(".");
+                }
+            }
+            finally
+            {
+                Monitor.Exit(c);
+            }
+            /*    
+            lock (c)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(r.Next(2000));
+                    Message += "B";
+                    Write(".");
+                }
+            }
+            */
+        }
+    }
+}
